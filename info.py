3e545e32d101b7e574519809dd5e76b963325334
Manish Jadhav <54273848+ManishJadhav100@users.noreply.github.com>
2022-03-27 10:33:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,16 +12,14 @@ BOT_TOKEN = '5274443907:AAHgHcPGT3FvcjHnd0p4thWIi_3zSNed8fE'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [1394463981]
 CHANNELS = [-1001490115011]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+AUTH_USERS = []
+AUTH_CHANNEL = None
 
 # MongoDB information
 DATABASE_URI = 'mongodb+srv://moviesearch:Manya@004@cluster0.p2e1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
